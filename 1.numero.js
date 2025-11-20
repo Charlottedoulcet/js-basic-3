db6b95882863adc7cce562bb718c8ca7c988232c
marie charlotte <charlottedoulcet90@gmail.com>
2025-11-20 18:29:27 +0100

    numbers exercices done

---
+++ 1.numero.js
@@ -0,0 +1,60 @@
+//Ejercicio 1//
+function sumar(num1, num2) {
+  return num1 + num2;
+}
+console.log(sumar(5, 5));
+
+//Ejercicio 2//
+function restar(num1, num2) {
+  return num1 - num2;
+}
+console.log(restar(5, 5));
+
+//Ejercicio 3//
+function producto(num1, num2) {
+  return num1 * num2;
+}
+console.log(producto(5, 5));
+
+//Ejercicio 4//
+function dividir(num1, num2) {
+  return num1 / num2;
+}
+console.log(dividir(10, 5));
+
+//Ejercicio 5//
+function potenciar(base, exponente) {
+  return base ** exponente;
+}
+console.log(potenciar(2, 3));
+
+//Ejercicio 6//
+function restoDivision(num1, num2) {
+  return num1 % num2;
+}
+console.log(restoDivision(10, 3));
+
+//Ejercicio 7//
+function raizCuadrada(numero) {
+  return Math.sqrt(numero);
+}
+console.log(raizCuadrada(16));
+Math.sq;
+
+//Ejercicio 8//
+function valorAbsoluto(num1) {
+  return Math.abs(num1);
+}
+console.log(valorAbsoluto(-10));
+
+//Ejercicio 9//
+function numDecimal(num1) {
+  return Math.round(num1);
+}
+console.log(numDecimal(16.89));
+
+//Ejercicio 10//
+function numAleatorio() {
+  return Math.random();
+}
+console.log(numAleatorio());
